3d9155fbf9b7845951595e1373eb399a6b126cff
Eilane Santos <>
2016-02-26 22:36:52 -0300

    Conexão Banco

--- agenda/src/main/java/br/senac/tads/pi3/eilane/agenda/Agenda.java
+++ agenda/src/main/java/br/senac/tads/pi3/eilane/agenda/Agenda.java
@@ -5,12 +5,32 @@
  */
 package br.senac.tads.pi3.eilane.agenda;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author temp.cas
  */
 public class Agenda {
     
+    private Connection obterConexao() throws SQLException, ClassNotFoundException{
+    Connection conn = null;
+    
+    // Passo 1: Regitrar driver JDBC 
+    Class.forName("org.apache.derby.jdbc.ClientDataSource");
+    
+    // Passo 2: Abrir a conexão 
+    conn = DriverManager.getConnection(           //SecurityMechanism - padrão que define que será informado usuário e senha
+            "jdbc:derby://localhost:1527/agendadb;SecurityMechanism=3",
+            "app", // usuario 
+            "app");// senha 
+    
+    return conn;
+    
+    
+    }
     
     
 }
