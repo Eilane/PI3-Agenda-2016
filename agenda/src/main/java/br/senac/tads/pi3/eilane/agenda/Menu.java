94cae3110c0e721e6cd1835d65f759a21c17a1ac
iosato <iosato@outlook.com>
2016-03-03 20:47:10 -0300

    Criada novas classe para separar responsabilidades e criado o menu a ser exibido

---
+++ agenda/src/main/java/br/senac/tads/pi3/eilane/agenda/Menu.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.pi3.eilane.agenda;
+
+/**
+ *
+ * @author iosato
+ */
+public class Menu {
+    
+    private int escolha;
+    
+    
+    // Construtor
+    public Menu() {
+    }
+    
+    
+    //Getter e Setters
+    public void setEscolha(int escolha) {
+        this.escolha = escolha;
+    }
+
+    public int getEscolha() {
+        return escolha;
+    }
+    
+    // Exibe menu e lê a opção escolhida
+    public int menuInicial(Scanner input, Opcao o) {   //chama o logo inicial
+
+        System.out.println("");
+        System.out.println("#################################################################");
+        System.out.println("#################################################################");
+        System.out.println("############################## MENU #############################");
+        System.out.println("#################################################################");
+        System.out.println("#################### (1)  Buscar Contato     ####################");
+        System.out.println("#################### (2)  Cadastrar Contato  ####################");
+        System.out.println("#################### (3)  Editar Contato     ####################");
+        System.out.println("#################### (4)  Excluir Contato    ####################");
+        System.out.println("#################### (0)  Sair               ####################");
+        System.out.println("#################################################################");
+        System.out.println("#################################################################");
+        System.out.println("#################################################################");
+        System.out.println("");
+
+        do {
+            System.out.print("Digite a opção desejada: ");
+            this.setEscolha(input.nextInt());
+            System.out.println("");
+        } while (this.getEscolha() < 0 || this.getEscolha() > 4);
+        return this.getEscolha();
+    }
+    
+    
+}
